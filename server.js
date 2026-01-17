cc813185f4e26699911c63082d798b7618dfcd87
v2trapz7 <rubegeurts62@gmail.com>
2026-01-17 18:49:01 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-import 'dotenv/config';
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// 👉 THIS serves your website
 app.use(express.static(path.join(__dirname, "public")));
 
 const app = express();
